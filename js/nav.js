bc6f56990c15011d0b65890353de354c20b66410
Mcouple <suplzy0409@gmail.com>
2022-01-06 13:26:23 +0800

    1.6revise

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
     var lis = $("li[id='li_main']"),
         divs = $("div[id='nav_bar']");
     for (var i = 0; i < lis.length; i++) {
@@ -14,20 +13,6 @@ $(document).ready(function() {
             })
         })(i)
 
-=======
-
-    let lis = $("#nav").find("li")
-    for (let i = 0; i < lis.length; i++) {
-        $(lis[i]).mouseenter(function() {
-            $(lis[i]).addClass("nav_active")
-            $(lis[i]).find("div").slideDown(100);
-
-            $(lis[i]).mouseleave(function() {
-                $(lis[i]).removeClass("nav_active")
-                $(lis[i]).find("div").slideUp(100);
-            })
-        })
->>>>>>> 0daff70e78dc527cb4e5177f3a9613bc4e1b0232
     }
 
     //导航栏加背景颜色
@@ -40,10 +25,4 @@ $(document).ready(function() {
             $(".nav_t").removeClass("s")
         }
     });
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 0daff70e78dc527cb4e5177f3a9613bc4e1b0232
 })
